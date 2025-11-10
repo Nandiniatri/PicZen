38622cfe01e7b4548436983e6b77b3730b327846
Nandiniatri <atrinandini8@gmail.com>
2025-11-10 15:25:27 +0530

    make a button and input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,7 @@
+const Input = ({ type , className, value, onChange, placeholder }) => {
+    return (
+        <input type={type} className={className} value={value} onChange={onChange} placeholder={placeholder}/>
+    );
+}   
+
+export default Input;
