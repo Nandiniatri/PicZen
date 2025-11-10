a1d2b8fed7f420e73d6e2fccb47c363a60a5d277
Nandiniatri <atrinandini8@gmail.com>
2025-11-11 00:42:36 +0530

    change name of logo

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,29 +1,3 @@
-// import React from "react";
-// import "./Header.css";
-// import Button from "../../components/Button";
-
-// const Header = () => {
-//   return (
-//     <header className="header">
-//       <h1 className="logo">Logo</h1>
-
-//       <nav className="nav">
-//         <a href="#" className="nav-link">Product</a>
-//         <a href="#" className="nav-link">Solution</a>
-//       </nav>
-
-//       <div className="btn-group">
-//         <Button>Log in</Button>
-//         <Button>Start creating</Button>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
-
-
 import "./Header.css";
 import Button from "../../components/Button";
 
@@ -33,7 +7,7 @@ const Header = () => {
     <header className="header">
       <div className="logo">
         <div className="logo-icon">◎</div>
-        <h2>idea AI</h2>
+        <h2>PICZEN</h2>
       </div>
 
       <nav className="nav">
