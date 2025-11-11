37c51a922babea03f811d8caee741a411a671214
Nandiniatri <atrinandini8@gmail.com>
2025-11-11 17:02:40 +0530

    my homePage5 done

--- src/pages/Piczen.jsx
+++ src/pages/Piczen.jsx
@@ -3,6 +3,7 @@ import Home from "./home/Home";
 import HomePage2 from "./home/homePage2/HomePage2";
 import HomePage3Food from "./home/homePage3/HomePage3Food";
 import HomePage4Food from "./home/homePage4/HomePage4Food";
+import HomePage5 from "./home/homePage5/HomePage5";
 
 
 const PicZen = () => {
@@ -14,6 +15,7 @@ const PicZen = () => {
             <HomePage2 />
             <HomePage3Food />
             <HomePage4Food />
+            <HomePage5 />
         </div>
     </div>
   );
---
+++ src/pages/home/homePage5/HomePage5.jsx
@@ -0,0 +1,43 @@
+import "./HomePage5.css";
+
+export default function HomePage5() {
+  return (
+    <div className="testimonial-container">
+      {/* Card 1 */}
+      <div className="tc-card">
+        <div className="tc-tag">SMB</div>
+
+        <div className="tc-img-wrapper">
+          <img
+            src="https://storyblok-cdn.photoroom.com/f/191576/1176x448/4e35601b00/source_the_watches_inc.webp"
+            alt="Watch"
+            className="tc-main-img"
+          />
+        </div>
+
+        <div className="tc-content">
+          <div className="tc-avatar-row">
+            <img
+              src="https://storyblok-cdn.photoroom.com/f/191576/144x176/55f66a7b6b/source_the_watches_inc.webp"
+              alt="avatar"
+              className="tc-avatar"
+            />
+          </div>
+
+          <p className="tc-text">
+            “With Piczen Background Remover and AI Enhancer, we’ve seen the
+            number of product inquiries jump by 40 percent, boosting sales
+            leads from 20 percent to 60 percent per post.”
+          </p>
+
+          <div className="tc-author">
+            <h4>Saad Basir</h4>
+            <p>Founder, Watches Inc.</p>
+          </div>
+
+          <button className="tc-btn">Read story</button>
+        </div>
+      </div>
+    </div>
+  );
+}
