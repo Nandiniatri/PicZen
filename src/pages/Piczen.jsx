a35799a7f04aeced0acd0dc6cebd163cd88ed32c
Nandiniatri <atrinandini8@gmail.com>
2025-11-11 11:27:56 +0530

    done home page 2

--- src/pages/Piczen.jsx
+++ src/pages/Piczen.jsx
@@ -1,5 +1,6 @@
 import Header from "./header/Header";
 import Home from "./home/Home";
+import HomePage2 from "./home/homePage2/HomePage2";
 
 
 const PicZen = () => {
@@ -8,6 +9,7 @@ const PicZen = () => {
         {/* <Header /> */}
         <div>
             <Home />
+            <HomePage2 />
         </div>
     </div>
   );
