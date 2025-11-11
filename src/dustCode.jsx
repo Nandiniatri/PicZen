5ab29cd88bd2866b5e782491d544299a1dd19808
Nandiniatri <atrinandini8@gmail.com>
2025-11-11 11:50:20 +0530

    resuable code make and usme mene data rakh diya hai

---
+++ src/dustCode.jsx
@@ -0,0 +1,46 @@
+// import './FeatureSection.css';
+
+// const FeatureSection = ({}) => {
+//     return (
+//         <div className="hp2-wrapper">
+//             <div className="hp2-title-block">
+//                 <h1 className="hp2-title">{hpTitle}</h1>
+//                 <h1 className="hp2-title-flex">
+//                     <h1 className="hp2-you">{hpYou}</h1>
+//                     {icon && <img
+//                         src={hpImage1}
+//                         className="hp2-icon"
+//                     />}
+
+//                     {hpText3}
+//                 </h1>
+//             </div>
+
+//             <div className="hp2-content">
+
+//                 <div className="hp2-image-block">
+//                     <img
+//                         src={hpImage2}
+//                         className="hp2-main-img"
+//                     />
+//                 </div>
+
+//                 <div className="hp2-text-block">
+//                     <h2 className="hp2-heading">
+//                         {hpHeading}
+//                     </h2>
+
+//                     <p className="hp2-desc">
+//                         {hpDesc}
+//                     </p>
+
+//                     <Button className="hp2-btn">{hpButtonText}</Button>
+//                 </div>
+
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default FeatureSection;
+
