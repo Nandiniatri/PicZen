5aa4c304bcccd3e692f815f0415e768332b6e4f1
Nandiniatri <atrinandini8@gmail.com>
2025-11-12 14:30:34 +0530

    import mirage js in main file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AppEventProvider from './contextApi/AppEventContext.jsx'
+import { makeServer } from './mirageJs/server.js'
 
+
+makeServer();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppEventProvider>
