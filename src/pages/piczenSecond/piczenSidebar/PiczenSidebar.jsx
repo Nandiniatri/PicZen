67140ddcd2573e00c5666ea8dfc49b68c73e3329
Nandiniatri <atrinandini8@gmail.com>
2025-11-13 02:32:03 +0530

    css lag gayi hai

--- src/pages/piczenSecond/piczenSidebar/PiczenSidebar.jsx
+++ src/pages/piczenSecond/piczenSidebar/PiczenSidebar.jsx
@@ -1,26 +1,80 @@
+// import { useAppEvent } from "../../../contextApi/AppEventContext";
+
+// const PiczenSidebar = () => {
+//     const { menus, contents } = useAppEvent();
+//     console.log(menus);
+
+
+//     return (
+//         <div>
+//             <div className="logo">
+//                 <div className="logo-icon">◎</div>
+//                 <h2>PICZEN</h2>
+//             </div>
+
+//             <ul>
+//                 {menus.map(item => (
+//                     <li key={item.id}>{item.name} ({item.icon})</li>
+//                 ))}
+//             </ul>
+
+//             <ul>
+//                 {contents.map(item => (
+//                     <li key={item.id}>{item.title}</li>
+//                 ))}
+//             </ul>
+
+//         </div>
+//     )
+// }
+
+// export default PiczenSidebar;
+
+
 import { useAppEvent } from "../../../contextApi/AppEventContext";
+import { Home, Cpu, Layers, Clock } from "lucide-react"; // icons
+
+const iconMap = {
+    home: <Home size={18} />,
+    cpu: <Cpu size={18} />,
+    layers: <Layers size={18} />,
+    clock: <Clock size={18} />,
+};
 
 const PiczenSidebar = () => {
     const { menus, contents } = useAppEvent();
-    console.log(menus);
-
 
     return (
-        <div>
-            <ul>
-                {menus.map(item => (
-                    <li key={item.id}>{item.name} ({item.icon})</li>
-                ))}
-            </ul>
+        <aside className="sidebar">
+            <div className="logo">
+                <div className="logo-icon">◎</div>
+                <h2>PICZEN</h2>
+            </div>
 
-            <ul>
-                {contents.map(item => (
-                    <li key={item.id}>{item.title}</li>
-                ))}
-            </ul>
+            <nav className="menu-section">
+                <ul>
+                    {menus.map((item) => (
+                        <li key={item.id} className="menu-item">
+                            <span className="menu-icon">{iconMap[item.icon]}</span>
+                            <span className="menu-text">{item.name}</span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
 
-        </div>
-    )
-}
+            <div className="divider"></div>
+
+            <nav className="content-section">
+                <ul>
+                    {contents.map((item) => (
+                        <li key={item.id} className="menu-item">
+                            <span className="menu-text">{item.title}</span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </aside>
+    );
+};
 
 export default PiczenSidebar;
