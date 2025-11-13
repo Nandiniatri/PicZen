996a1ae64ce0961f3af5c9b21580fe184eff0d7b
Nandiniatri <atrinandini8@gmail.com>
2025-11-13 13:05:56 +0530

    make a piczenRightSideData

--- src/pages/piczenSecond/piczenRightSide/PiczenRightSide.jsx
+++ src/pages/piczenSecond/piczenRightSide/PiczenRightSide.jsx
@@ -1,8 +1,33 @@
+import Button from "../../../components/Button";
+import Input from "../../../components/Input";
+import './PiczenRightSide.css';
+
 const PiczenRightSide = () => {
   return (
-    <>  
-        <h2>Piczen Right Side</h2>
-    </>
+    <div>
+      <div>
+        <h2>Create</h2>
+        <Input type="search" placeholder="Search a template" />
+      </div>
+
+      <div className="getStarted-main-div">
+        <div>
+          <p>Get started</p>
+          <Button>See all</Button>
+        </div>
+
+        <div>
+          
+        </div>
+
+
+      </div>
+
+
+      <div>
+
+      </div>
+    </div>
   )
 }
 export default PiczenRightSide; 
