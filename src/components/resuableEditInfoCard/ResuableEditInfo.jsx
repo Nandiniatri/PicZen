9543570f054722254c793bb7bfa52e45451407f3
Nandiniatri <atrinandini8@gmail.com>
2025-11-14 14:50:00 +0530

    make a resuable componnet

--- src/components/resuableEditInfoCard/ResuableEditInfo.jsx
+++ src/components/resuableEditInfoCard/ResuableEditInfo.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./ResuableEditInfo.css";
 
-const ResuableEditInfo = () => {
+const ResuableEditInfo = ({ src, data }) => {
     return (
         <>
             <div className="remove-card-img">
                 <img
-                    src="https://cdn.pixabay.com/photo/2016/11/25/09/47/sun-flower-1857997_1280.png"
+                    src={src}
                     alt="Remove Background"
                 />
             </div>
             <div className="remove-card-text">
-                <p>Background Remove</p>
+                <p>{data}</p>
             </div>
         </>
     );
