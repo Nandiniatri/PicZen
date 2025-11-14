dc183def177989a57fcfcc60cb4fddc3b78c2f1f
Nandiniatri <atrinandini8@gmail.com>
2025-11-14 16:04:16 +0530

    done all the ResuableEdit Info dat

--- src/pages/piczenSecond/piczenRightSide/PiczenRightSide.jsx
+++ src/pages/piczenSecond/piczenRightSide/PiczenRightSide.jsx
@@ -23,14 +23,14 @@ const PiczenRightSide = () => {
         <div className="getStarted-header">
           <p className="getStarted-title">Get started</p>
           <Button className="seeAll-btn">See all</Button>
-        </div>
+        </div> 
 
         <div className="imagesDrop-div">
           <div className="image-drop-box imagedrop-div"><ImageSelect /></div>
-          <div className="image-drop-box"><ResuableEditInfo /></div>
-          <div className="image-drop-box"><ImageSelect /></div>
-          <div className="image-drop-box"><ImageSelect /></div>
-          <div className="image-drop-box"><ImageSelect /></div>
+          <div className="image-drop-box"><ResuableEditInfo src="https://cdn.pixabay.com/photo/2016/11/25/09/47/sun-flower-1857997_1280.png" data="Remove Background"/></div>
+          <div className="image-drop-box"><ResuableEditInfo data="AI Background" src="https://cdn.pixabay.com/photo/2024/08/02/10/40/picture-8939420_1280.jpg"/></div>
+          <div className="image-drop-box"><ResuableEditInfo data="Batch mode" src="https://cdn.pixabay.com/photo/2017/03/14/13/50/urban-art-2143183_960_720.jpg" /></div>
+          <div className="image-drop-box"><ResuableEditInfo data="Retouch" src="https://cdn.pixabay.com/photo/2023/11/13/17/25/women-8386031_960_720.jpg" /></div>
         </div>
       </div>
 
