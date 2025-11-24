f0cd267d4b3fddc920ca8a2f02830facf7f0b1e9
Nandiniatri <atrinandini8@gmail.com>
2025-11-24 14:54:08 +0530

    make a classisArrive css

--- src/pages/piczenSecond/piczenRightSide/classics/ClassicsArrive.jsx
+++ src/pages/piczenSecond/piczenRightSide/classics/ClassicsArrive.jsx
@@ -1,20 +1,27 @@
+import './ClassicsArrive.css';
+
 const ClassicsArrive = () => {
     const items = [
         {
             title: "White",
             image: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
+            bg: "#ffffff"
         },
         {
             title: "Black",
             image: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
+            bg: "#000000",
         },
         {
             title: "Transparent",
             image: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
+            bg: "transparent",
+            pattern: true,
         },
         {
             title: "Original Image",
             image: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
+            bgImage: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
         },
     ];
 
@@ -25,7 +32,17 @@ const ClassicsArrive = () => {
             <div className="classics-grid">
                 {items.map((item, index) => (
                     <div key={index} className="classics-card">
-                        <img src={item.image} alt={item.title} className="classic-img" />
+
+                        <div
+                            className={`classic-bg ${item.pattern ? "checker" : ""}`}
+                            style={{
+                                backgroundColor: item.bg,
+                                backgroundImage: item.bgImage ? `url(${item.bgImage})` : undefined,
+                            }}
+                        >
+                            <img src={item.image} alt={item.title} className="classic-img" />
+                        </div>
+
                         <p className="classic-label">{item.title}</p>
                     </div>
                 ))}
