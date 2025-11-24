1231105ae69d5a40582e92faddd45f9aa2f65857
Nandiniatri <atrinandini8@gmail.com>
2025-11-24 15:04:55 +0530

    json data

--- src/pages/piczenSecond/piczenRightSide/classics/ClassicsArrive.jsx
+++ src/pages/piczenSecond/piczenRightSide/classics/ClassicsArrive.jsx
@@ -1,36 +1,15 @@
+import AppEventProvider from '../../../../contextApi/AppEventContext';
 import './ClassicsArrive.css';
 
 const ClassicsArrive = () => {
-    // const items = [
-    //     {
-    //         title: "White",
-    //         image: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
-    //         bg: "#ffffff"
-    //     },
-    //     {
-    //         title: "Black",
-    //         image: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
-    //         bg: "#000000",
-    //     },
-    //     {
-    //         title: "Transparent",
-    //         image: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
-    //         bg: "transparent",
-    //         pattern: true,
-    //     },
-    //     {
-    //         title: "Original Image",
-    //         image: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
-    //         bgImage: "https://cdn.photoroom.com/v2/image-cache?path=gs%3A%2F%2Fbackground-7ef44.appspot.com%2Ftemplates_v2%2F675515b8-83d8-45e0-a3ec-7e8c8366ced0%2Fthumb_7C11634D-9BB5-4D1C-932B-06C176EEADE0.jpg",
-    //     },
-    // ];
+    const { classicArrive } = AppEventProvider();
 
     return (
         <div className="classics-container">
             <h2 className="classics-title">Classics</h2>
 
             <div className="classics-grid">
-                {items.map((item, index) => (
+                {classicArrive.map((item, index) => (
                     <div key={index} className="classics-card">
 
                         <div
