e11a363923f08987daaf7b0c825b10569dc9f253
Nandiniatri <atrinandini8@gmail.com>
2025-12-02 23:41:00 +0530

    add background component

---
+++ src/pages/edits/addBackground/BackgroundModal.jsx
@@ -0,0 +1,34 @@
+import "./AddBackground.css";
+import { Wand2, Square, Image, Trash2 } from "lucide-react";
+
+const BackgroundModal = ({ onClose }) => {
+    return (
+        <div className="bg-menu-container">
+            <ul className="bg-menu-list">
+
+                <li className="bg-menu-item">
+                    <Wand2 size={18} />
+                    <span>AI Backgrounds</span>
+                </li>
+
+                <li className="bg-menu-item">
+                    <Square size={18} />
+                    <span>Solid color</span>
+                </li>
+
+                <li className="bg-menu-item">
+                    <Image size={18} />
+                    <span>Image</span>
+                </li>
+
+                <li className="bg-menu-item remove">
+                    <Trash2 size={18} />
+                    <span>Remove background</span>
+                </li>
+
+            </ul>
+        </div>
+    );
+};
+
+export default BackgroundModal;
