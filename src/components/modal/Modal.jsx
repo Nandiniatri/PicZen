70b7a5bb1c8345719d35aa173aa87bd987a9cce3
Nandiniatri <atrinandini8@gmail.com>
2025-12-11 13:31:15 +0530

    make a modal componets

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,21 @@
+const Modal = ({ open, onClose, title, children }) => {
+    if (!open) return null;
+
+
+    return (
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal-box" onClick={(e) => e.stopPropagation()}>
+                <div className="modal-header">
+                    <h3>{title}</h3>
+                    <button className="close-btn" onClick={onClose}>×</button>
+                </div>
+
+                <div className="modal-body">{children}</div>
+
+                <div className="modal-footer">
+                    <button className="btn" onClick={onClose}>Close</button>
+                </div>
+            </div>
+        </div>
+    );
+};
