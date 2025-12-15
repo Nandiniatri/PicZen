417be91523984fe0f6191d8cf5ccb558851fc69f
Nandiniatri <atrinandini8@gmail.com>
2025-12-15 16:11:52 +0530

    make a python program file in backend

---
+++ backend/remove_subject.py
@@ -0,0 +1,13 @@
+from rembg import remove
+import sys
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+with open(input_path, 'rb') as f:
+    input_data = f.read()
+
+output_data = remove(input_data)
+
+with open(output_path, 'wb') as f:
+    f.write(output_data)
