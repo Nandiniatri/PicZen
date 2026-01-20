1d7f48b0c85af9addb8f1324f697bffe06004477
Nandiniatri <atrinandini8@gmail.com>
2026-01-20 13:13:56 +0530

    data not come

--- src/components/multipleAIPages/aIModal/AIModalComponent.jsx
+++ src/components/multipleAIPages/aIModal/AIModalComponent.jsx
@@ -6,18 +6,29 @@ import { useAppEvent } from "../../../contextApi/AppEventContext";
 
 const AIModalComponent = ({ open, onClose }) => {
     const { selectedGenItem } = useAppEvent();
+    console.log(selectedGenItem);
+    
 
     if (!open) return null;
+    // if(!selectedGenItem) {
+    //     return null;
+    // }
 
     return (
         <ModalPortal>
             <div className="AImodal-overlay" onClick={onClose}>
                 <div className="AIModal-container" onClick={(e) => e.stopPropagation()}>
                     <div className="AIModal-header">
-                        <h1>Hello</h1>
                         <button className="modal2-close-btn" onClick={onClose}>
                             ✕
                         </button>
+                    </div>
+
+                    <div>
+                        {/* {selectedGenItem && selectedGenItem.map((AIItem) => {
+                            console.log(AIItem);
+                            
+                        })} */}
                     </div>
                 </div>
             </div>
